63e5669c3df744a649142753c9640142982e0e63
ANONYMOUS <iamcicada3301@gmail.com>
2017-11-15 18:39:20 +0530

    added new activity : SubscriberDetails

---
+++ app/src/main/java/com/example/anonymous/librarian/SubscriberDetails.java
@@ -0,0 +1,13 @@
+package com.example.anonymous.librarian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubscriberDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscriber_details);
+    }
+}
