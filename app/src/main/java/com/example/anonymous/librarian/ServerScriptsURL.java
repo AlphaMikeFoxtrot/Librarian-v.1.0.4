07887bb2d3c032144bc221f8388316a45c47c8df
ANONYMOUS <iamcicada3301@gmail.com>
2017-12-15 17:15:16 +0530

    added new url class to handle php file urls properly

---
+++ app/src/main/java/com/example/anonymous/librarian/ServerScriptsURL.java
@@ -0,0 +1,179 @@
+package com.example.anonymous.librarian;
+
+/**
+ * Created by ANONYMOUS on 15-Dec-17.
+ */
+
+public class ServerScriptsURL {
+
+    public final String ADD_BOOK = "http://fardeenpanjwani.com/";
+    public final String ADD_SUBSCRIBER = "http://fardeenpanjwani.com/";
+    public final String ADD_TOY = "http://fardeenpanjwani.com/";
+    public final String CHECK_PROFILE_PHOTO = "http://fardeenpanjwani.com/";
+    public final String CONNECTION = "http://fardeenpanjwani.com/";
+    public final String DELETE_BOOK = "http://fardeenpanjwani.com/";
+    public final String DELETE_SUBSCRIBER = "http://fardeenpanjwani.com/";
+    public final String DELETE_TOY = "http://fardeenpanjwani.com/";
+    public final String GET_BOOK_DETAILS = "http://fardeenpanjwani.com/";
+    public final String GET_INDIVIDUAL_ANALYSIS = "http://fardeenpanjwani.com/";
+    public final String GET_INDIVIDUAL_SUBSCRIBER_DETAILS = "http://fardeenpanjwani.com/";
+    public final String GET_ISSUED_BOOKS = "http://fardeenpanjwani.com/";
+    public final String GET_SINGLE_ISSUED_BOOK_DETAILS = "http://fardeenpanjwani.com/";
+    public final String GET_SUBSCRIBERS_DETAILS = "http://fardeenpanjwani.com/";
+    public final String GET_SUBSCRIBER_ANALYSIS = "http://fardeenpanjwani.com/";
+    public final String GET_TEMP_BOOK_DETAILS = "http://fardeenpanjwani.com/";
+    public final String GET_TEMP_TOY_DETAILS = "http://fardeenpanjwani.com/";
+    public final String GET_TOTAL_ANALYSIS = "http://fardeenpanjwani.com/";
+    public final String GET_TOY_DETAILS = "http://fardeenpanjwani.com/";
+    public final String INSERT_TEMP_BOOK_DETAILS = "http://fardeenpanjwani.com/";
+    public final String INSERT_TEMP_TOY_DETAILS = "http://fardeenpanjwani.com/";
+    public final String ISSUE_BOOK = "http://fardeenpanjwani.com/";
+    public final String ISSUE_TOY = "http://fardeenpanjwani.com/";
+    public final String LAST_DAY_PROTOCOL = "http://fardeenpanjwani.com/";
+    public final String RETURN_BOOK = "http://fardeenpanjwani.com/";
+    public final String RETURN_TOY = "http://fardeenpanjwani.com/";
+    public final String UPDATE_SUBSCRIBER_DETAILS = "http://fardeenpanjwani.com/";
+    public final String UPLOAD_SUBSCRIBER_PROFILE_IMAGE_ENHANCED = "http://fardeenpanjwani.com/";
+    public final String UPLOAD_SUBSCRIBER_PROFILE_PHOTO = "http://fardeenpanjwani.com/";
+    public final String VIEW_CURRENTLY_ISSUED_TOYS = "http://fardeenpanjwani.com/";
+    public final String CANCEL_ISSUE_BOOK_PROTOCOL = "http://fardeenpanjwani.com/";
+    public final String CANCEL_ISSUE_TOY_PROTOCOL = "http://fardeenpanjwani.com/";
+
+    private String mUserCenter;
+
+    public ServerScriptsURL(String mUserCenter) {
+        this.mUserCenter = mUserCenter;
+    }
+
+    public String getADD_BOOK() {
+        return ADD_BOOK;
+    }
+
+    public String getADD_SUBSCRIBER() {
+        return ADD_SUBSCRIBER;
+    }
+
+    public String getADD_TOY() {
+        return ADD_TOY;
+    }
+
+    public String getCHECK_PROFILE_PHOTO() {
+        return CHECK_PROFILE_PHOTO;
+    }
+
+    public String getCONNECTION() {
+        return CONNECTION;
+    }
+
+    public String getDELETE_BOOK() {
+        return DELETE_BOOK;
+    }
+
+    public String getDELETE_SUBSCRIBER() {
+        return DELETE_SUBSCRIBER;
+    }
+
+    public String getDELETE_TOY() {
+        return DELETE_TOY;
+    }
+
+    public String getGET_BOOK_DETAILS() {
+        return GET_BOOK_DETAILS;
+    }
+
+    public String getGET_INDIVIDUAL_ANALYSIS() {
+        return GET_INDIVIDUAL_ANALYSIS;
+    }
+
+    public String getGET_INDIVIDUAL_SUBSCRIBER_DETAILS() {
+        return GET_INDIVIDUAL_SUBSCRIBER_DETAILS;
+    }
+
+    public String getGET_ISSUED_BOOKS() {
+        return GET_ISSUED_BOOKS;
+    }
+
+    public String getGET_SINGLE_ISSUED_BOOK_DETAILS() {
+        return GET_SINGLE_ISSUED_BOOK_DETAILS;
+    }
+
+    public String getGET_SUBSCRIBERS_DETAILS() {
+        return GET_SUBSCRIBERS_DETAILS;
+    }
+
+    public String getGET_SUBSCRIBER_ANALYSIS() {
+        return GET_SUBSCRIBER_ANALYSIS;
+    }
+
+    public String getGET_TEMP_BOOK_DETAILS() {
+        return GET_TEMP_BOOK_DETAILS;
+    }
+
+    public String getGET_TEMP_TOY_DETAILS() {
+        return GET_TEMP_TOY_DETAILS;
+    }
+
+    public String getGET_TOTAL_ANALYSIS() {
+        return GET_TOTAL_ANALYSIS;
+    }
+
+    public String getGET_TOY_DETAILS() {
+        return GET_TOY_DETAILS;
+    }
+
+    public String getINSERT_TEMP_BOOK_DETAILS() {
+        return INSERT_TEMP_BOOK_DETAILS;
+    }
+
+    public String getINSERT_TEMP_TOY_DETAILS() {
+        return INSERT_TEMP_TOY_DETAILS;
+    }
+
+    public String getISSUE_BOOK() {
+        return ISSUE_BOOK;
+    }
+
+    public String getISSUE_TOY() {
+        return ISSUE_TOY;
+    }
+
+    public String getLAST_DAY_PROTOCOL() {
+        return LAST_DAY_PROTOCOL;
+    }
+
+    public String getRETURN_BOOK() {
+        return RETURN_BOOK;
+    }
+
+    public String getRETURN_TOY() {
+        return RETURN_TOY;
+    }
+
+    public String getUPDATE_SUBSCRIBER_DETAILS() {
+        return UPDATE_SUBSCRIBER_DETAILS;
+    }
+
+    public String getUPLOAD_SUBSCRIBER_PROFILE_IMAGE_ENHANCED() {
+        return UPLOAD_SUBSCRIBER_PROFILE_IMAGE_ENHANCED;
+    }
+
+    public String getUPLOAD_SUBSCRIBER_PROFILE_PHOTO() {
+        return UPLOAD_SUBSCRIBER_PROFILE_PHOTO;
+    }
+
+    public String getVIEW_CURRENTLY_ISSUED_TOYS() {
+        return VIEW_CURRENTLY_ISSUED_TOYS;
+    }
+
+    public String getCANCEL_ISSUE_BOOK_PROTOCOL() {
+        return CANCEL_ISSUE_BOOK_PROTOCOL;
+    }
+
+    public String getCANCEL_ISSUE_TOY_PROTOCOL() {
+        return CANCEL_ISSUE_TOY_PROTOCOL;
+    }
+
+    public String getmUserCenter() {
+        return mUserCenter;
+    }
+}
